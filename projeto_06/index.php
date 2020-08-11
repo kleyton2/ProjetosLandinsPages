2b06e6e7f0d5619823efe9bd4daffc2d441e8018
Kleyton Gonçalves <58863668+kleyton2@users.noreply.github.com>
2020-08-11 15:30:56 -0300

    Add files via upload

---
+++ projeto_06/index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Meu Projeto 06</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+<body>
+	
+	<section class="main">
+		<div class="layer"></div>
+		<div class="center">
+			<header>
+				<div class="icons-social">
+					<a href="https://facebook.com"><i class="fab fa-facebook"></i></a>
+					<a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
+				</div><!--icons-->
+				<div class="clear"></div>
+				<div class="logo">
+					Kleyton.Lucas	
+				</div><!--logo-->	
+				<nav class="desktop">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Contato</a></li>
+					<li><a href="">Sobre</a></li>
+				</ul>
+				</nav><!--deskop-->
+				<nav class="mobile">
+				<i class="fa fa-bars"></i>
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Contato</a></li>
+					<li><a href="">Sobre</a></li>
+				</ul>
+
+
+				</nav>
+
+				<div class="clear"></div>
+			</header>
+			<div class="conteudo-header">
+				<h2>Bem vindo ao nosso <span>Website!</span></h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pulvinar, nulla quis semper molestie, odio eros ultricies est, at tempor dui nulla sit amet velit. Praesent dignissim enim vel nisi tristique tristique. Aliquam vel risus elit. Aenean at ipsum at purus viverra fringilla. Donec elementum odio quis suscipit pharetra. Nullam placerat luctus velit. Donec mattis leo ut accumsan suscipit. Suspendisse potenti. In consequat, odio sit amet faucibus mattis, nunc diam bibendum dui, a gravida neque lorem id ante..</p>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pulvinar, nulla quis semper molestie, odio eros ultricies est, at tempor dui nulla sit amet velit. Praesent dignissim enim vel nisi tristique tristique. Aliquam vel risus elit. Aenean at ipsum at purus viverra fringilla. Donec elementum odio quis suscipit pharetra.</p>
+				<a target="_blank" href="https://google.com">Saiba mais!</a>
+			</div><!--conteudo-header-->
+		</div><!--center-->		
+	</section><!--main-->
+
+	<section class="planos">
+	   <div class="chamada-planos">		
+		<div class="center">
+			<h2>Confira nossos planos!</h2>
+		</div><!--center-->
+	   </div><!--chamada-planos-->
+	   <section class="box-planos">
+	   		<div class="center">
+	   			<div class="box-planos-single">
+	   			<div class="box-planos-wraper">
+	   				<h2>Titulo da oferta</h2>
+	   				<div class="pontos-planos">
+	   					<p><i class="fas fa-check-square"></i>plano de oferta 1</p>
+	   					<p><i class="fas fa-check-square"></i>plano de oferta 2</p>
+	   					<p><i class="fas fa-check-square"></i>plano de oferta 3</p>
+	   					<p><i class="fas fa-check-square"></i>plano de oferta 4</p>
+	   				</div><!--pontos-planos-->
+	   				<div class="preco">
+	   					<h2>R$250,00</h2>
+	   				</div><!--preco-->
+	   			</div><!--box-plano-wraper-->	   				
+	   			</div><!--planos-single-->
+	   			<div class="box-planos-single">
+	   			<div class="box-planos-wraper">
+	   				<h2>Titulo da oferta</h2>
+	   				<div class="pontos-planos">
+	   					<p><i class="fas fa-check-square"></i>plano de oferta 1</p>
+	   					<p><i class="fas fa-check-square"></i>plano de oferta 2</p>
+	   					<p><i class="fas fa-check-square"></i>plano de oferta 3</p>
+	   					<p><i class="fas fa-check-square"></i>plano de oferta 4</p>
+	   				</div><!--pontos-planos-->
+	   				<div class="preco">
+	   					<h2>R$250,00</h2>
+	   				</div><!--preco-->
+	   			</div><!--box-plano-wraper-->	   				
+	   			</div><!--planos-single-->
+	   			<div class="box-planos-single">
+	   			<div class="box-planos-wraper">
+	   				<h2>Titulo da oferta</h2>
+	   				<div class="pontos-planos">
+	   					<p><i class="fas fa-check-square"></i>plano de oferta 1</p>
+	   					<p><i class="fas fa-check-square"></i>plano de oferta 2</p>
+	   					<p><i class="fas fa-check-square"></i>plano de oferta 3</p>
+	   					<p><i class="fas fa-check-square"></i>plano de oferta 4</p>
+	   				</div><!--pontos-planos-->
+	   				<div class="preco">
+	   					<h2>R$250,00</h2>
+	   				</div><!--preco-->
+	   			</div><!--box-plano-wraper-->	   				
+	   			</div><!--planos-single-->
+	   		</div><!--center-->
+	   </section><!--box-planos-->
+	</section><!--planos-->
+
+	<section class="paralax">
+		<div class="overlay-paralax"></div>
+		<div class="center">
+			<h2>Sobre Nossa <span style="color: #2692ff;">empresa</span></h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pulvinar, nulla quis semper molestie, odio eros ultricies est, at tempor dui nulla sit amet velit. Praesent dignissim enim vel nisi tristique tristique. Aliquam vel risus elit. Aenean at ipsum at purus viverra fringilla. Donec elementum odio quis suscipit pharetra. Nullam placerat luctus velit. Donec mattis leo ut accumsan suscipit. Suspendisse potenti.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pulvinar, nulla quis semper molestie, odio eros ultricies est, at tempor dui nulla sit amet velit. Praesent dignissim enim vel nisi tristique tristique. Aliquam vel risus elit. Aenean at ipsum at purus viverra fringilla. Donec elementum odio quis suscipit pharetra. Nullam placerat luctus velit. Donec mattis leo ut accumsan suscipit. Suspendisse potenti.</p>
+		</div><!--center-->
+	</section>
+
+	<section class="final-section">
+		<div class="center">
+			<div class="w50 contato">
+				<h2><div class="borda-efeito"></div>Entre em contato!</h2>
+				<form>
+					<input type="text" placeholder="ensira seu nome:" name="">
+					<input type="email" placeholder="ensira seu e-mail:" name="">
+					<textarea placeholder="deixe uma mensagem!"></textarea>
+					<input type="submit" name="" value="Enviar">
+				</form>
+			</div>
+		<div class="w50 time">
+			<h2><div class="borda-efeito"></div> Conheça nosso time!</h2>
+			<div class="w50 equipe-single">
+				<div class="topo-box-usuario">
+				<div class="box-avatar"></div><div class="info-usuario">
+					<p style="font-weight: bold;">Kleyton lucas</p>
+					<p style="font-weight: italic;">CEO / Instrutor</p>
+				</div>
+				</div><!--topo-box-->
+				<div class="descricao-usuario-box">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pulvinar, nulla quis semper molestie, odio eros ultricies est, at tempor dui nulla sit amet velit. Praesent dignissim enim vel nisi tristique tristique. Aliquam vel risus elit.</p>
+				</div><!--descricao-usuario-box-->
+			</div>
+			<div class="w50 equipe-single">
+				<div class="topo-box-usuario">
+				<div class="box-avatar"></div><div class="info-usuario">
+					<p style="font-weight: bold;">Kleyton lucas</p>
+					<p style="font-weight: italic;">CEO / Instrutor</p>
+				</div>
+				</div><!--topo-box-->
+				<div class="descricao-usuario-box">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pulvinar, nulla quis semper molestie, odio eros ultricies est, at tempor dui nulla sit amet velit. Praesent dignissim enim vel nisi tristique tristique. Aliquam vel risus elit.</p>
+				</div><!--descricao-usuario-box-->
+			</div>
+			<div class="clear"></div>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</section>
+	<footer>
+		<p style="border-bottom: 1px solid black;">Todos os direitos resrvados!</p>
+	</footer>
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js
+"></script>
+<script type="text/javascript">
+	$(function(){
+		$('nav.mobile i').click(function(){
+			var el = $(this).parent().find('ul');
+			if(el.is(':visible') == false){
+				el.fadeIn();
+			}else{
+				el.fadeOut();
+			}
+		})
+	})
+
+</script>
+
+</body>
+</html>
